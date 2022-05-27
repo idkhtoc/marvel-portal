5e8797f45289687d3b9d165cd79b7a089759dca0
Alex <older.miller.ua@gmail.com>
2022-05-27 23:26:00 +0200

    Errors and loading functional add for Random Character

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,19 @@
+import errorGif from './error.gif';
+
+const ErrorMessage = () => {
+    return (
+        <img 
+            src={errorGif} 
+            alt="Error gif" 
+            style={{
+                display: 'block',
+                width: '250px',
+                height: '250px',
+                objectFit: 'contain',
+                margin: '0 auto'
+            }} 
+        />
+    );
+}
+
+export default ErrorMessage;
