ceabd2af49b065f79a03a973677185282f7de2ea
Oleksandr Yanov <oleksandr.yanov.eu@gmail.com>
2024-05-30 20:09:57 +0200

    Updates

---
+++ assets/ErrorMessage-BJBaawzR.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Cuvk2aXt.js";const s="/assets/error-BBH_usxQ.gif",o=()=>r.jsx("img",{src:s,alt:"Error gif",style:{display:"block",width:"250px",height:"250px",objectFit:"contain",margin:"0 auto"}});export{o as E};
