1d63ed4ae472fcff3c0fe576fa6dc14e914e2962
Alex Yanov <older.miller.ua@gmail.com>
2024-06-11 15:08:47 +0200

    Updates

--- assets/ErrorMessage-OuOfxM8k.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-Dog9rYPr.js";const s="/assets/error-BBH_usxQ.gif",o=()=>r.jsx("img",{src:s,alt:"Error gif",style:{display:"block",width:"250px",height:"250px",objectFit:"contain",margin:"0 auto"}});export{o as E};
